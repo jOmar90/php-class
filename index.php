8fe3d1fa453c2cfc190c76c774d4794ba5969d6e
mJomar90 <mjomar1990@gmail.com>
2025-11-16 15:45:59 +0100

    Replace index.php with updated homepage

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
-<?php
-
-echo "Hello, PHP is working!";
-
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <title>Library Management System</title>
+    <meta charset="UTF-8">
+    </head>
+<body>
+    <h1>Welcome to the Library Management System</h1>
+    <p>This system allows you to manage authors and books.</p>
+    <ul>
+        <li><a href="add_author.php">Add Author</a></li>
+        <li><a href="add_book.php">Add Book</a></li>
+        <li><a href="show_authors.php">Show All Authors</a></li>
+        <li><a href="show_books.php">Show All Books</a></li>
+        <li><a href="search_by_author.php">Show Books by Author</a></li>
+        <li><a href="remove_author.php">Remove Author</a></li>
+        <li><a href="remove_book.php">Remove Book</a></li>
+    </ul>
+</body>
+</html>
